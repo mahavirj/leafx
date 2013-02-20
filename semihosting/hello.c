6cfe59b577bf82dbb6b90e67d72dcb3bc55b098f
Mahavir Jain <mahavirpj@gmail.com>
2013-02-20 12:27:50 +0530

    semihosting: simple cortex-m3 semihosting app
    Signed-off-by: Mahavir Jain <mahavirpj@gmail.com>

---
+++ semihosting/hello.c
@@ -0,0 +1,17 @@
+/*
+ * Author: Mahavir Jain <mahavirpj@gmail.com>
+ *
+ * This file has been put into the public domain.
+ * You can do whatever you want with this file.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("*** Start semihosted application demo\n");
+	printf("Hello world\n");
+	printf("*** Application done\n");
+	return 0;
+}
+
