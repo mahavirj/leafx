007543b10600763cff72427ad64388b6d70ceeed
Mahavir Jain <mahavirpj@gmail.com>
2016-12-11 13:24:20 +0530

    Experimental wait queue framework
    Signed-off-by: Mahavir Jain <mahavirpj@gmail.com>

---
+++ code_snippets/libs/wait_queue.c
@@ -0,0 +1,66 @@
+#include <wait_queue.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int wait_queue_deinit(wq_handle *handle)
+{
+	if (!handle || !*handle)
+		return -EINVAL;
+	free((void *)*handle);
+	*handle = 0;
+	return 0;
+}
+
+int wait_queue_init(wq_handle *handle)
+{
+	if (!handle)
+		return -EINVAL;
+	wq_t *w = (wq_t *) malloc(sizeof(wq_t));
+	if (!w)
+		return -ENOMEM;
+	
+	w->obj_cnt = 0;
+	INIT_LIST_HEAD(&w->list);
+
+	*handle = (wq_handle) w;
+	return 0;
+}
+
+int wait_queue_insert(wq_handle handle, list_head_t *node)
+{
+	if (!handle || !node)
+		return -EINVAL;
+	wq_t *w = (wq_t *) handle;
+
+	list_add_tail(node, &w->list);
+	w->obj_cnt++;
+	return 0;
+}
+
+list_head_t *wait_queue_remove(wq_handle handle)
+{
+	list_head_t *node;
+
+	if (!handle)
+		return NULL;
+	wq_t *w = (wq_t *) handle;
+	
+	if (list_empty(&w->list))
+		return NULL;
+
+	w->obj_cnt--;
+	list_for_each(node, &w->list) {
+		list_del(node);
+		return node;
+	}
+	return NULL;
+}
+
+int wait_queue_objectse(wq_handle handle)
+{
+	if (!handle)
+		return -EINVAL;
+	wq_t *w = (wq_t *) handle;
+	return w->obj_cnt;
+}
